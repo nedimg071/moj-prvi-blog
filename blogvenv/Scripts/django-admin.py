89166668a53acf73fa3fa059eea5b96fbcedf99e
nedimg <nedimg@bih.net.ba>
2018-10-07 19:42:36 +0200

    Moja prva django blog app, prvi commit

---
+++ blogvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\nedim\business\e-actuary\django-learning\django-blog\blogvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
