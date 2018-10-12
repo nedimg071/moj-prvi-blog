43763e7ea9647027ddf9411705df491c844d149e
nedimg <nedimg@bih.net.ba>
2018-10-12 22:48:44 +0200

    additional files koji ce se prenijeti na Heroku

--- blogNGsite/wsgi.py
+++ blogNGsite/wsgi.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
-from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogNGsite.settings')
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
