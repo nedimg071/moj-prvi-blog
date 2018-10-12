0b0b7294feaeeac95c70cc244b7bbc3c1d0797c4
nedimg <nedimg@bih.net.ba>
2018-10-12 22:49:26 +0200

    additional files koji ce se prenijeti na Heroku

--- blogNGsite/settings.py
+++ blogNGsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'aq9t^%10&z)0_mkpvvssq^4yf8x_dg0b11(j$s_28k@6m*x_#e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
